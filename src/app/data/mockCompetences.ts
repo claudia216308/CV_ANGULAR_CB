3a79a6e240f17ebf8cf3cdcf2e8db6607997d297
claudia216308 <216308@supinfo.com>
2020-11-08 22:49:17 +0100

    update competence mock

--- src/app/data/mockCompetences.ts
+++ src/app/data/mockCompetences.ts
@@ -2,20 +2,21 @@ import { Competence } from "../modele/competence";
 
 
 export const FRONT: Competence[] = [
-  { title: 'Angular', percentage: 80 },
+  { title: 'Javascript', percentage: 80 },
   { title: 'HTML5', percentage: 60 },
   { title: 'CSS3', percentage: 45 },
 ];
 
 export const BACK: Competence[] = [
   { title: 'Java', percentage: 80 },
-  { title: 'C++', percentage: 60 },
+  { title: 'Scala', percentage: 60 },
   { title: 'Android', percentage: 45 },
   { title: 'PHP', percentage: 45 },
+  { title: 'Python', percentage: 45 },
 ];
 
-
 export const FRAME: Competence[] = [
-  { title: 'Hibernate', percentage: 80 },
+  { title: 'Angular', percentage: 80 },
+  { title: 'Hibernate', percentage: 70 },
   { title: 'Spring', percentage: 60 },
 ];
